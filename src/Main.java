40210b5ced42b26297b11ada612f097593ab1700
bassemAbd <sasem3@gmail.com>
2022-12-20 19:49:06 -0500

    completed feature planet

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        solarSystem Xron=new solarSystem();
+        SolarSystem Xron=new SolarSystem();
         Xron.moon=2;
         Xron.planets=2;
         Xron.sun=1;
@@ -11,6 +11,14 @@ public class Main {
         Fxron.color="green";
         Fxron.size="4382742km";
         Fxron.temp="-23478274372k";
+
+        Planet1 stuart = new Planet1();
+
+        stuart.color="white";
+        stuart.size=23423;
+        stuart.rings="5";
+        stuart.temperature="-4884848k";
+
 
     }
 }
