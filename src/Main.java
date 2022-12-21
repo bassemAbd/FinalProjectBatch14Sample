ae8c36f5025e9b9060b0e1fc38867b5d902416c6
bassemAbd <sasem3@gmail.com>
2022-12-20 20:11:26 -0500

    Planet2 added and working fine

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        solarSystem Xron=new solarSystem();
+        SolarSystem Xron=new SolarSystem();
         Xron.moon=2;
         Xron.planets=2;
         Xron.sun=1;
@@ -11,6 +11,12 @@ public class Main {
         Fxron.color="green";
         Fxron.size="4382742km";
         Fxron.temp="-23478274372k";
+
+        Planet2 Boris=new Planet2();
+        Boris.color="pink";
+        Boris.size=3498;
+        Boris.rings="1";
+        Boris.temperature="9238k";
 
     }
 }
